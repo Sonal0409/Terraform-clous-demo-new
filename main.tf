9be904f878b9ee9f70e9ac82f641bc701176ef05
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2024-12-04 15:31:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+data "aws_ami" "myami" {
+
+most_recent      = true
+
+owners           = ["amazon"]
+
+ filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+
+}
+
+
+resource "aws_instance" "myec2" {
+
+  ami           = data.aws_ami.myami.id
+  instance_type = "t2.micro"
+}
+
