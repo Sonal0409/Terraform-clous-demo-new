9e5e22b42c4fc8dbb4785ad32bbf6d6ec6134ce9
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2024-12-04 16:00:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,8 +21,9 @@ resource "aws_instance" "myec2" {
 
   ami           = data.aws_ami.myami.id
   instance_type = "t2.micro"
-tags = {
-Name = "instance1"
-}
+ tags = {
+    Name = "Instance1"
+  }
+
 }
 
